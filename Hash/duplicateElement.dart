751bd2ea8e72657a132169467e0406598b99a102
Jay Isampelliwar <isampelliwar79@gmail.com>
2022-06-11 18:42:26 +0530

    Hash is added

---
+++ Hash/duplicateElement.dart
@@ -0,0 +1,13 @@
+void main(List<String> args) {
+  var arr = [1, 6, 4, 3, 2, 8, 6];
+  var set = HashSet<int>();
+
+  arr.forEach((element) {
+    if (!set.isEmpty && set.contains(element)) {
+      print(element);
+      return;
+    } else {
+      set.add(element);
+    }
+  });
+}
